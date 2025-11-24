5d4def60aa1a23df68b927aef7a460293d2e6047
Adi Kusuma <adiikusuma1001@gmail.com>
2025-11-24 11:16:56 +0700

    DEPLOYMENT

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,13 @@
 import sys
+import os
 import asyncio
 import uvicorn
 
 if __name__ == "__main__":
     if sys.platform.startswith("win"):
-        print("Mengaktifkan WindowsProactorEventLoopPolicy...")
         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
-    print("Menjalankan Server Stable (Reload Non-Aktif)...")
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    
+    print(f"Menjalankan Server di Port {port}...")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
